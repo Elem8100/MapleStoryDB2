99a97e24db0a144a7fba3b367fb294a0dbd03491
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2021-06-20 07:48:22 +0800

    Delete MapleStoryDB2 directory

--- MapleStoryDB2/CharaSimControl/ItemMouseEventHandler.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WzComparerR2.CharaSimControl
-{
-    public delegate void ItemMouseEventHandler(object sender, ItemMouseEventArgs e);
-}
--- MapleStoryDB2/WzComparerR2.Common/GlobalFindNodeFunction.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using WzComparerR2.WzLib;
-
-namespace WzComparerR2
-{
-    public delegate Wz_Node GlobalFindNodeFunction(string fullPath);
-}
