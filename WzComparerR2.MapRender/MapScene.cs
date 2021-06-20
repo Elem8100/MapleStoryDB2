7656d6c1c042d28bc15a3e5ea308ca2e209f370c
Elem8100 <49195287+Elem8100@users.noreply.github.com>
2021-06-20 08:43:39 +0800

    Add files via upload

---
+++ WzComparerR2.MapRender/MapScene.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using WzComparerR2.MapRender.Patches2;
+
+namespace WzComparerR2.MapRender
+{
+    public class MapScene : SceneNode
+    {
+        public MapScene()
+        {
+            this.Nodes.AddRange(new SceneNode[] {
+                Back = new ContainerNode(),
+                Layers = new SceneNode(),
+                Fly = new FlyLayerNode(),
+                Front = new ContainerNode(),
+                Effect = new ContainerNode()
+            });
+
+            for (int i = 0; i <= 7; i++)
+            {
+                this.Layers.Nodes.Add(new LayerNode());
+            }
+        }
+
+        public ContainerNode Back { get; private set; }
+        public SceneNode Layers { get; private set; }
+        public FlyLayerNode Fly { get; private set; }
+        public ContainerNode Front { get; private set; }
+        public ContainerNode Effect { get; private set; }
+
+        public IEnumerable<PortalItem> Portals => this.Fly.Portal.Slots.OfType<PortalItem>();
+
+        public PortalItem FindPortal(string pName)
+        {
+            return this.Portals.FirstOrDefault(_portal => _portal.PName == pName);
+        }
+    }
+
+    public class LayerNode : SceneNode
+    {
+        public LayerNode()
+        {
+            this.Nodes.AddRange(new SceneNode[]
+            {
+                Obj = new ContainerNode(),
+                Reactor = new ContainerNode(),
+                Tile = new ContainerNode(),
+                Foothold = new SceneNode()
+            });
+        }
+
+        public ContainerNode Obj { get; private set; }
+        public ContainerNode Reactor { get; private set; }
+        public ContainerNode Tile { get; private set; }
+        public SceneNode Foothold { get; private set; }
+    }
+
+    public class FlyLayerNode : SceneNode
+    {
+        public FlyLayerNode()
+        {
+            this.Nodes.AddRange(new SceneNode[]
+            {
+                Portal = new ContainerNode(),
+                LadderRope = new ContainerNode(),
+                Sky = new ContainerNode(),
+                SkyWhale = new ContainerNode(),
+            });
+        }
+
+
+        public ContainerNode Portal { get; private set; }
+        public ContainerNode LadderRope { get; private set; }
+        /// <summary>
+        /// 表示不属于任何Foothold的虚拟节点。
+        /// </summary>
+        public ContainerNode Sky { get; private set; }
+        public ContainerNode SkyWhale { get; private set; }
+    }
+
+    public class ContainerNode : SceneNode
+    {
+        public ContainerNode()
+        {
+            this.Slots = new List<SceneItem>();
+        }
+
+        public List<SceneItem> Slots { get; private set; }
+    }
+
+    public class ContainerNode<T> : ContainerNode
+    {
+        public T Item { get; set; }
+    }
+}
---
+++ WzComparerR2.MapRender/Music.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WzComparerR2.WzLib;
+using System.Runtime.InteropServices;
+using Un4seen.Bass;
+
+namespace WzComparerR2.MapRender
+{
+    class Music : IDisposable
+    {
+        public Music(Wz_Sound sound)
+        {
+            this.soundData = sound.ExtractSound();
+            this.pData = GCHandle.Alloc(this.soundData, GCHandleType.Pinned);
+            this.hStream = Bass.BASS_StreamCreateFile(pData.AddrOfPinnedObject(), 0, this.soundData.Length, BASSFlag.BASS_DEFAULT);
+            Music.GlobalVolumeChanged += this.OnGlobalVolumeChanged;
+        }
+
+        private byte[] soundData;
+        private GCHandle pData;
+        private int hStream;
+        private float? vol;
+
+        public bool IsLoop
+        {
+            get { return (Bass.BASS_ChannelFlags(hStream, 0, 0) & BASSFlag.BASS_SAMPLE_LOOP) != 0; }
+            set { Bass.BASS_ChannelFlags(hStream, value ? BASSFlag.BASS_SAMPLE_LOOP : BASSFlag.BASS_DEFAULT, BASSFlag.BASS_SAMPLE_LOOP); }
+        }
+
+        public PlayState State
+        {
+            get
+            {
+                var active = Bass.BASS_ChannelIsActive(hStream);
+                switch (active)
+                {
+                    case BASSActive.BASS_ACTIVE_STOPPED: return PlayState.Stopped;
+                    case BASSActive.BASS_ACTIVE_PLAYING: return PlayState.Playing;
+                    case BASSActive.BASS_ACTIVE_PAUSED: return PlayState.Paused;
+                    default: return PlayState.Unknown;
+                }
+            }
+        }
+
+        public float Volume
+        {
+            get
+            {
+                if (vol == null)
+                {
+                    float value = 0;
+                    vol = Bass.BASS_ChannelGetAttribute(hStream, BASSAttribute.BASS_ATTRIB_VOL, ref value) ? value : 0;
+                }
+                return vol.Value;
+            }
+            set
+            {
+                vol = value;
+                Bass.BASS_ChannelSetAttribute(hStream, BASSAttribute.BASS_ATTRIB_VOL, vol.Value * globalVol);
+            }
+        }
+
+        public void Play()
+        {
+            Bass.BASS_ChannelPlay(hStream, false);
+        }
+
+        public void Pause()
+        {
+            Bass.BASS_ChannelPause(hStream);
+        }
+
+        public void Stop()
+        {
+            Bass.BASS_ChannelStop(hStream);
+        }
+
+        public void Dispose()
+        {
+            Music.GlobalVolumeChanged -= this.OnGlobalVolumeChanged;
+            Bass.BASS_StreamFree(hStream);
+            this.pData.Free();
+
+        }
+
+        public enum PlayState
+        {
+            Stopped = 0,
+            Playing = 1,
+            Paused = 2,
+
+            Unknown = -1,
+        }
+
+        private void OnGlobalVolumeChanged(object sender, EventArgs e)
+        {
+            this.Volume = Volume;
+        }
+
+        #region Global Volume
+        private static float globalVol = 1f;
+        private static event EventHandler GlobalVolumeChanged;
+        public static float GlobalVolume
+        {
+            get { return globalVol; }
+            set
+            {
+                globalVol = value;
+                GlobalVolumeChanged?.Invoke(null, EventArgs.Empty);
+            }
+        }
+        #endregion
+    }
+}
